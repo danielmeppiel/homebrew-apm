9503f3baa6a79509a304aeb90f8d1aab57f3e215
GitHub Action <action@github.com>
2025-08-28 08:42:04 +0000

    Update APM CLI to vv0.1.7

--- Formula/apm-cli.rb
+++ Formula/apm-cli.rb
@@ -1,18 +1,18 @@
 class AwdCli < Formula
   desc "Agent Primitives Manager (APM): The NPM for AI-Native Development"
   homepage "https://github.com/danielmeppiel/apm-cli"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   if Hardware::CPU.arm? && OS.mac?
     url "https://github.com/danielmeppiel/apm-cli/releases/download/v#{version}/apm-darwin-arm64.tar.gz"
-    sha256 "e8d9844cbc76aa33e928b2a4bb2ca13d518de7d5b7184b525a7868b65080c109"
+    sha256 "2e2d97d760d05b47ef3cde8ef9706248586b5e06d1045578dfefb6fe53015f8b"
   elsif Hardware::CPU.intel? && OS.mac?
     url "https://github.com/danielmeppiel/apm-cli/releases/download/v#{version}/apm-darwin-x86_64.tar.gz"
-    sha256 "d4d82c054e184aac4fb76752dcf154f524452915c20d7f5438db2365438c8371"
+    sha256 "e8aa7273d879e24f3a524004b3da6ac512b8bea6d061fe7705758b0862ae4af5"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/danielmeppiel/apm-cli/releases/download/v#{version}/apm-linux-x86_64.tar.gz"
-    sha256 "4044762c58451417c67c450746b377dec67a7ebe0f3d2aab6202f0f28e5268dc"
+    sha256 "9017b1e02a6f26c5dee79cab2ca919800daf3e5ff56a6d877194aa1d4461ff21"
   end
 
   def install
